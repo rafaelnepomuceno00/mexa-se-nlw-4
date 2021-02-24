51bfee82031871f012426ec79ec3ae594fe87d94
Rafael <rafaelnepomuceno00@gmail.com>
2021-02-24 13:29:08 -0300

    inserindo o next.js

---
+++ src/components/Profile.tsx
@@ -0,0 +1,14 @@
+export function Profile(){
+    return(
+        <div>
+
+        <img src="https://github.com/rafaelnepomuceno00.png" alt='Rafael Nepomuceno'/>
+        <div>
+            <strong>
+                Rafael Nepomuceno
+            </strong>
+            <p>Level 1</p>
+        </div>
+        </div>
+    )
+}
---
+++ src/pages/index.tsx
@@ -0,0 +1,24 @@
+import { profile } from 'console'
+import Head from 'next/head'
+import { ExperienceBar } from "../components/ExperienceBar"
+import { Profile } from '../components/Profile'
+
+export default function Home() {
+
+    return (
+
+        <div className='container'>
+           
+            <ExperienceBar />
+            <section>
+                <div >
+            <Profile/>
+                </div>
+                <div>
+
+                </div>
+            </section>
+        </div>
+
+    )
+} 
