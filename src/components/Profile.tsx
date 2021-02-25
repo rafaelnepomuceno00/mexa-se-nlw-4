b2ab19c9ac953bf2ada9c4bda5f0ca9aeadb7467
Rafael <rafaelnepomuceno00@gmail.com>
2021-02-25 00:40:47 -0300

    Adcionando estilo a página inicial e adicionando botão e função countdown

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,14 +1,18 @@
-export function Profile(){
-    return(
-        <div>
+import styles from '../styles/components/Profile.module.css'
+export function Profile() {
+    return (
+        <div className={styles.profileContainer}>
 
-        <img src="https://github.com/rafaelnepomuceno00.png" alt='Rafael Nepomuceno'/>
-        <div>
-            <strong>
-                Rafael Nepomuceno
+            <img src="https://github.com/rafaelnepomuceno00.png" alt='Rafael Nepomuceno' />
+            <div>
+                <strong>
+                    Rafael Nepomuceno
             </strong>
-            <p>Level 1</p>
-        </div>
+                <p>
+                    <img src="icons/level.svg" alt="level" />
+                    Level 1
+                </p>
+            </div>
         </div>
     )
 }
